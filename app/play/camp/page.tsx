54a4786ce8602a821a55ad0f1df8f29bdf623b35
Max Freundlich <mfreundlich1@gmail.com>
2023-10-03 22:53:05 -0400

    fix: scrollable sheet content

--- app/play/camp/page.tsx
+++ app/play/camp/page.tsx
@@ -80,7 +80,10 @@ export default function CampPages() {
                 <SheetTrigger asChild>
                   <Button>View Inventory</Button>
                 </SheetTrigger>
-                <SheetContent side="bottom" className="w-100% h-[100dvh]">
+                <SheetContent
+                  side="bottom"
+                  className="w-100% h-[100dvh] flex flex-col pb-0"
+                >
                   <SheetHeader>
                     <SheetTitle>Inventory</SheetTitle>
                     <SheetDescription>
@@ -88,12 +91,17 @@ export default function CampPages() {
                       just be junk, or they might be useful.
                     </SheetDescription>
                   </SheetHeader>
-                  <div className="grid grid-cols-3 sm:grid-cols-6 md:grid-cols-9 lg:grid-cols-12 gap-5 mt-8 ">
-                    {[...Array(30)].map((_, i) => (
-                      <div key={i} className="flex items-center justify-center">
-                        <div className="border rounded-md border-foreground/20 h-12 aspect-square" />
-                      </div>
-                    ))}
+                  <div className="flex w-full overflow-hidden">
+                    <div className="w-full grid grid-cols-3 sm:grid-cols-6 md:grid-cols-9 lg:grid-cols-12 gap-5 mt-8 pb-8 overflow-scroll">
+                      {[...Array(30)].map((_, i) => (
+                        <div
+                          key={i}
+                          className="flex items-center justify-center"
+                        >
+                          <div className="border rounded-md border-foreground/20 h-12 aspect-square" />
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 </SheetContent>
               </Sheet>
