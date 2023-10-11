0ce6ad25e7ddedb708628a9ec5e98d890081e194
Max Freundlich <mfreundlich1@gmail.com>
2023-10-11 13:32:08 -0400

    unique id for chat

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -36,7 +36,7 @@ export async function POST(req: Request) {
 
   // Adapt the Streamship Blockstream into a Markdown Stream
   const stream = await SteamshipStream(response, steamship, {
-    streamTimeoutSeconds: 30,
+    streamTimeoutSeconds: 60,
     // Use: "markdown" | "json"
     format: "json",
   });
--- components/quest/quest.tsx
+++ components/quest/quest.tsx
@@ -1,15 +1,26 @@
+"use client";
 import { QuestContainer } from "@/components/quest/shared/components";
 import QuestNarrative from "./quest-narrative";
 import { GameState } from "@/lib/game/schema/game_state";
 import { QuestHeader } from "./quest-header";
-import { useId } from "react";
+import { useEffect, useState } from "react";
+import { v4 } from "uuid";
 
 export default function Quest({ gameState }: { gameState: GameState }) {
-  const id = useId();
+  const [id, setId] = useState<string | null>(null);
+
+  useEffect(() => {
+    setId(v4());
+  }, []);
+
   return (
     <QuestContainer>
-      <QuestHeader gameState={gameState} id={id} />
-      <QuestNarrative id={id} />
+      {id && (
+        <>
+          <QuestHeader gameState={gameState} id={id} />
+          <QuestNarrative id={id} />
+        </>
+      )}
     </QuestContainer>
   );
 }
