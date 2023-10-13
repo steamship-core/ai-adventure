5101a2a142ca84a69160a9cf885e4dad8acb3ba6
Max Freundlich <mfreundlich1@gmail.com>
2023-10-13 15:36:28 -0400

    debugging streaming

--- components/quest/quest-narrative/streaming-block.tsx
+++ components/quest/quest-narrative/streaming-block.tsx
@@ -4,20 +4,25 @@ import { DebugBlock } from "./debug-blocks";
 import { useBlockStream } from "./use-block-stream";
 import { TextBlock } from "./text-block";
 import { useDebugMode } from "@/lib/hooks";
+import { cn } from "@/lib/utils";
+import { TypographySmall } from "@/components/ui/typography/TypographySmall";
 
 export const StreamingBlock = ({ block }: { block: Block }) => {
   const { streamedBlock } = useBlockStream({ block });
   const { isDebugMode } = useDebugMode();
 
-  if (!isDebugMode) {
-    return (
-      <DebugBlock
-        block={streamedBlock}
-        title="Streaming"
-        className="border-indigo-600"
-      />
-    );
-  }
+  // if (!isDebugMode) {
+  return (
+    <div
+      className={cn(
+        "p-2 border border-indigo-600 rounded-md opacity-70 text-sm"
+      )}
+    >
+      <TypographySmall>Streaming</TypographySmall>
+      <p>{streamedBlock.text}</p>
+    </div>
+  );
+  // }
 
-  return <TextBlock text={streamedBlock.text!} />;
+  // return <TextBlock text={streamedBlock.text!} />;
 };
