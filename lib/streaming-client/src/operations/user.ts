54eda01b14d97bf3b374892ddf60bc9d22b00022
Ted Benson <ted@steamship.com>
2023-10-12 21:07:26 -0400

    Get base url

---
+++ lib/streaming-client/src/operations/user.ts
@@ -0,0 +1,28 @@
+/**
+ * Parameters for creating an instance of an agent.
+ *
+ */
+import {
+  Client,
+  AgentInstance,
+  IPackageClient,
+  PackageInstance,
+} from "../schema";
+import { IUserClient, User } from "../schema/user";
+
+export class UserClient implements IUserClient {
+  private client: Client;
+
+  constructor(client: Client) {
+    this.client = client;
+  }
+
+  public async current(): Promise<User> {
+    let response = await this.client.get(`account/current`, {});
+    let json = await response.json();
+    return json as User;
+  }
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default UserClient;
