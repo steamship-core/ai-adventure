68f899def3ae3738ef26832f04e52838b57c8f73
Ted Benson <ted@steamship.com>
2023-10-13 16:21:02 -0400

    Logs

--- app/api/block/[blockId]/route.ts
+++ app/api/block/[blockId]/route.ts
@@ -1,11 +1,14 @@
 import Steamship from "@/lib/streaming-client/src";
 import { getSteamshipClient } from "@/lib/utils";
+import { log } from "next-axiom";
 
 const GET = async (req: Request, context: { params: any }) => {
   const blockId = context.params.blockId;
+  log.info(`/api/block/${blockId}`);
   const steamship = getSteamshipClient();
-
+  log.info(`client config ${JSON.stringify(steamship.config)}`);
   const response = await steamship.block.raw({ id: blockId });
+  log.info(`response ok ${response.ok}`);
   return response;
 };
 
