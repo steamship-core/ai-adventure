95f727364d0bac20b05a042ff05532950c8662b3
Max Freundlich <mfreundlich1@gmail.com>
2023-10-20 15:36:19 -0400

    fix: aspect ratio

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -10,33 +10,15 @@ export async function GET(request: Request) {
 
   return new ImageResponse(
     (
-      <div
-        style={{
-          display: "flex",
-          fontSize: 60,
-          color: "black",
-          background: "#f6f6f6",
-          width: "100%",
-          height: "100%",
-          paddingTop: 50,
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <img
-          width="512"
-          height="512"
-          src={`https://api.staging.steamship.com/api/v1/block/${blockId}/raw`}
-          style={{
-            borderRadius: 128,
-          }}
-        />
-      </div>
+      <img
+        width="512"
+        height="512"
+        src={`https://api.staging.steamship.com/api/v1/block/${blockId}/raw`}
+      />
     ),
     {
-      width: 1200,
-      height: 630,
+      width: 512,
+      height: 512,
     }
   );
 }
