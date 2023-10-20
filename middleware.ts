06a9bfa558717ba025a7f30eaef895d77343b090
Max Freundlich <mfreundlich1@gmail.com>
2023-10-20 13:19:01 -0400

    logging to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,28 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
   publicRoutes: ["/", "/api/webhooks/stripe", "/share/quest", "/_axiom/logs"],
+  afterAuth(auth, req, evt) {
+    console.log("user", auth.userId);
+
+    // handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+    // redirect them to organization selection page
+    if (
+      auth.userId &&
+      !auth.orgId &&
+      req.nextUrl.pathname !== "/org-selection"
+    ) {
+      const orgSelection = new URL("/org-selection", req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+  },
 });
 
 export const config = {
