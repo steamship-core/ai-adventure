8822940e83ed67d9c6a4be8021830e23484a36ce
Max Freundlich <mfreundlich1@gmail.com>
2023-10-20 13:45:49 -0400

    fix: remove org selection

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,10 @@
-import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
+import { authMiddleware } from "@clerk/nextjs";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
   publicRoutes: ["/", "/api/webhooks/stripe", "/share/quest", "/_axiom/logs"],
-  afterAuth(auth, req, evt) {
-    console.log("user", auth.userId);
-    console.log("user obj", auth);
-    console.log("user str", JSON.stringify(auth));
-
-    // handle users who aren't authenticated
-    if (!auth.userId && !auth.isPublicRoute) {
-      return redirectToSignIn({ returnBackUrl: req.url });
-    }
-    // redirect them to organization selection page
-    if (
-      auth.userId &&
-      !auth.orgId &&
-      req.nextUrl.pathname !== "/org-selection"
-    ) {
-      const orgSelection = new URL("/org-selection", req.url);
-      return NextResponse.redirect(orgSelection);
-    }
-  },
 });
 
 export const config = {
