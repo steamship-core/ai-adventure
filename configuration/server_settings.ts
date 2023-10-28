7eb282c883a1a9dd26f40f60eb1f0398cf728292
Ted Benson <ted@steamship.com>
2023-10-27 20:21:18 -0400

    WIP

--- configuration/server_settings.ts
+++ configuration/server_settings.ts
@@ -1,110 +1,109 @@
-import { Prompt } from "@/lib/game/schema/prompt";
-import { ServerSettings } from "@/lib/game/schema/server_settings";
-import CampImagePrompt from "./image_prompts/camp_image_prompt";
+// import { Prompt } from "@/lib/game/schema/prompt";
+// import { ServerSettings } from "@/lib/game/schema/server_settings";
+// import CampImagePrompt from "./image_prompts/camp_image_prompt";
 
-function prompt_to_string(prompt: Prompt): string {
-  let value = prompt.value;
-  if (prompt.newlines_to_spaces === true) {
-    value = value.replaceAll("\n", " ");
-  }
-  value = value.trim();
-  return value;
-}
+// function prompt_to_string(prompt: Prompt): string {
+//   let value = prompt.value;
+//   if (prompt.newlines_to_spaces === true) {
+//     value = value.replaceAll("\n", " ");
+//   }
+//   value = value.trim();
+//   return value;
+// }
 
-/**
- * These settings are pushed into the game engine upon new game creation.
- *
- * See lib/game/schema/server_settings.ts for documentation.
- */
-const server_settings: ServerSettings = {
-  // STORY GENERATION
-  // ****************************************************************************************************
+// /**
+//  * These settings are pushed into the game engine upon new game creation.
+//  *
+//  * See lib/game/schema/server_settings.ts for documentation.
+//  */
+// const server_settings: ServerSettings = {
+//   // STORY GENERATION
+//   // ****************************************************************************************************
 
-  default_story_model: "gpt-4",
-  default_story_temperature: 0.4,
-  default_story_max_tokens: 256,
+//   default_story_model: "gpt-4",
+//   default_story_temperature: 0.4,
+//   default_story_max_tokens: 256,
 
-  // IMAGE GENERATION
-  // ****************************************************************************************************
+//   // IMAGE GENERATION
+//   // ****************************************************************************************************
 
-  /**
-   * A list of Stable Diffusion Image themes.
-   *
-   * Including your own custom theme will let you apply it to image prompts.
-   *
-   */
-  image_themes: [
+//   /**
+//    * A list of Stable Diffusion Image themes.
+//    *
+//    * Including your own custom theme will let you apply it to image prompts.
+//    *
+//    */
+//   image_themes: [
 
+// z  ],
 
-  ],
+//   /**
+//    * The Stable Diffusion theme for generating camp images.
+//    *
+//    * Use a built-in theme: "pixel_art_1" | "pixel_art_2"
+//    *
+//    * Or reference one of the ones included in this file.
+//    */
+//   camp_image_theme: 'pixel_art_1',
+//   camp_image_prompt: '{tone} {genre} camp.',
+//   camp_image_negative_prompt: '',
 
-  /**
-   * The Stable Diffusion theme for generating camp images.
-   *
-   * Use a built-in theme: "pixel_art_1" | "pixel_art_2"
-   *
-   * Or reference one of the ones included in this file.
-   */
-  camp_image_theme: 'pixel_art_1',
-  camp_image_prompt: '{tone} {genre} camp.',
-  camp_image_negative_prompt: '',
+//   /**
+//    * The Stable Diffusion theme for generating item images.
+//    *
+//    * Use a built-in theme: "pixel_art_1" | "pixel_art_2"
+//    *
+//    * Or reference one of the ones included in this file.
+//    */
+//   item_image_theme?: 'pixel_art_1',
+//   item_image_prompt?: `16-bit retro-game sprite for an item in a hero's inventory.
+//   The items's name is: {name}.
+//   The item's description is: {description}.`
 
-  /**
-   * The Stable Diffusion theme for generating item images.
-   *
-   * Use a built-in theme: "pixel_art_1" | "pixel_art_2"
-   *
-   * Or reference one of the ones included in this file.
-   */
-  item_image_theme?: 'pixel_art_1',
-  item_image_prompt?: `16-bit retro-game sprite for an item in a hero's inventory.
-  The items's name is: {name}.
-  The item's description is: {description}.`
+//   /**
+//    * The Stable Diffusion theme for generating profile images.
+//    *
+//    * Use a built-in theme: "pixel_art_1" | "pixel_art_2"
+//    *
+//    * Or reference one of the ones included in this file.
+//    */
+//   profile_image_theme?: string;
+//   profile_image_prompt?: string;
+//   profile_image_negative_prompt?: string;
 
-  /**
-   * The Stable Diffusion theme for generating profile images.
-   *
-   * Use a built-in theme: "pixel_art_1" | "pixel_art_2"
-   *
-   * Or reference one of the ones included in this file.
-   */
-  profile_image_theme?: string;
-  profile_image_prompt?: string;
-  profile_image_negative_prompt?: string;
+//   /**
+//    * The Stable Diffusion theme for generating quest background images.
+//    *
+//    * Use a built-in theme: "pixel_art_1" | "pixel_art_2"
+//    *
+//    * Or reference one of the ones included in this file.
+//    */
+//   quest_background_theme?: string;
+//   quest_background_image_prompt?: string;
+//   quest_background_image_negative_prompt?: string;
 
-  /**
-   * The Stable Diffusion theme for generating quest background images.
-   *
-   * Use a built-in theme: "pixel_art_1" | "pixel_art_2"
-   *
-   * Or reference one of the ones included in this file.
-   */
-  quest_background_theme?: string;
-  quest_background_image_prompt?: string;
-  quest_background_image_negative_prompt?: string;
+//   // STORY NARRATION
+//   // ****************************************************************************************************
 
-  // STORY NARRATION
-  // ****************************************************************************************************
+//   /**
+//    * Narration Model
+//    *
+//    * Valid models are:
+//    *   - elevenlabs
+//    */
+//   default_narration_model: "elevenlabs",
 
-  /**
-   * Narration Model
-   *
-   * Valid models are:
-   *   - elevenlabs
-   */
-  default_narration_model: "elevenlabs",
+//   /**
+//    * Quest Energy Cost
+//    *
+//    * This is how much energy each quest depletes from a user.
+//    */
+//   quest_cost: 10,
 
-  /**
-   * Quest Energy Cost
-   *
-   * This is how much energy each quest depletes from a user.
-   */
-  quest_cost: 10,
+//   /**
+//    * Profile Image
+//    */
+//   camp_image_prompt: prompt_to_string(CampImagePrompt),
+// };
 
-  /**
-   * Profile Image
-   */
-  camp_image_prompt: prompt_to_string(CampImagePrompt),
-};
-
-module.exports = server_settings;
+// module.exports = server_settings;
--- lib/game/schema/server_settings.ts
+++ lib/game/schema/server_settings.ts
@@ -1,6 +1,54 @@
+import { Prompt } from "./prompt";
 import { StableDiffusionTheme } from "./stable_diffusion_theme";
 
+/**
+ * TODO: Available items?
+ */
+
 export type ServerSettings = {
+  // ADVENTURE SETTINGS
+  // ****************************************************************************************************
+
+  /**
+   * The number of quests to generate for a single adventure.
+   */
+  quests_per_adventure?: number;
+
+  /**
+   * Difficulty modifier.
+   *
+   * This modifier is a number that is multiplied AGAINST every "dice roll" the user makes.
+   * That means a value of:
+   *
+   * - 0.5 will halve the likelihood of player success
+   * - 2.0 will double the likelihood of player success
+   * - 0.0 will make every roll a failure
+   */
+  difficulty_modifier?: number;
+
+  /**
+   * The universe that the adventure takes place in. Write as if it follows an executive
+   * summary heading underneath the "WORLD DESCRIPTION" heading, such as:
+   *
+   *    A space station orbiting a black hole in the Andromeda galaxy. Not in contact with other stations
+   *    for years. The station is running out of food and oxygen. The station is in danger of being pulled
+   *    into the black hole and only has a few days left.
+   */
+  adventure_universe_description?: Prompt;
+
+  // QUEST SETTINGS
+  // ****************************************************************************************************
+
+  obstacles_per_quest?: number;
+
+  // LOCATION SETTINGS
+  // Customize game.
+  // ****************************************************************************************************
+
+  /**
+   * The number of challenges to encounter in a single quest.
+   */
+
   // STORY GENERATION
   // ****************************************************************************************************
 
@@ -13,17 +61,13 @@ export type ServerSettings = {
 
   /**
    * A list of Stable Diffusion Image themes.
-   *
    * Including your own custom theme will let you apply it to image prompts.
-   *
    */
   image_themes?: StableDiffusionTheme[];
 
   /**
    * The Stable Diffusion theme for generating camp images.
-   *
    * Use a built-in theme: "pixel_art_1" | "pixel_art_2"
-   *
    * Or reference one of the ones included in this file.
    */
   camp_image_theme?: string;
@@ -32,9 +76,7 @@ export type ServerSettings = {
 
   /**
    * The Stable Diffusion theme for generating item images.
-   *
    * Use a built-in theme: "pixel_art_1" | "pixel_art_2"
-   *
    * Or reference one of the ones included in this file.
    */
   item_image_theme?: string;
@@ -43,9 +85,7 @@ export type ServerSettings = {
 
   /**
    * The Stable Diffusion theme for generating profile images.
-   *
    * Use a built-in theme: "pixel_art_1" | "pixel_art_2"
-   *
    * Or reference one of the ones included in this file.
    */
   profile_image_theme?: string;
@@ -54,9 +94,7 @@ export type ServerSettings = {
 
   /**
    * The Stable Diffusion theme for generating quest background images.
-   *
    * Use a built-in theme: "pixel_art_1" | "pixel_art_2"
-   *
    * Or reference one of the ones included in this file.
    */
   quest_background_theme?: string;
@@ -64,9 +102,14 @@ export type ServerSettings = {
   quest_background_image_negative_prompt?: string;
 
   // STORY NARRATION
+  //
+  // One of the pre-made voice narration themes available by the story engine.
+  //
+  // Unlike the Stable Diffusion themes, these can only be selected --- but not created --- by the web
+  // client.
   // ****************************************************************************************************
 
-  default_narration_model?: string;
+  narration_theme?: string;
 
   // MUSIC GENERATION
   // ****************************************************************************************************
