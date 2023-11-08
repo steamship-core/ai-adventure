f2cd7bf16be62ba3d4298c0528b24173fed7c47e
Max Freundlich <mfreundlich1@gmail.com>
2023-11-08 14:56:39 -0500

    feat: debugging

--- lib/streaming-client/src/streaming/block-stream-to-block-json.ts
+++ lib/streaming-client/src/streaming/block-stream-to-block-json.ts
@@ -12,7 +12,22 @@ function BlockStreamToBlockJsonStream(): TransformStream<Block, Uint8Array> {
     transform(block: Block, controller) {
       const str = JSON.stringify(block) + "\n";
       log.debug(`Streaming ${str}`);
-      controller.enqueue(new TextEncoder().encode(str));
+      try {
+        const encodedStr = new TextEncoder().encode(str);
+        try {
+          controller.enqueue(encodedStr);
+        } catch (e) {
+          console.log("Failed to enqueue string", encodedStr);
+          console.log(e);
+          log.debug(`Failed to enqueue string ${encodedStr}`);
+          log.debug(e as unknown as string);
+        }
+      } catch (e) {
+        console.log("Failed to encode string", str);
+        console.log(e);
+        log.debug(`Failed to encode string ${str}`);
+        log.debug(e as unknown as string);
+      }
       // If this block signals termination, hang up!
       if (isStreamTerminatingBlock(block)) {
         log.debug(`Terminating Block`);
