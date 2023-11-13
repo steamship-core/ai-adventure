81ce1437265aeff74a4c6e9413173daf97c1dfaf
Max Freundlich <mfreundlich1@gmail.com>
2023-11-13 11:36:36 -0500

    feat: add amplitude

---
+++ lib/amplitude.ts
@@ -0,0 +1,34 @@
+"use client";
+
+import * as amplitudeBrowser from "@amplitude/analytics-browser";
+import { useUser } from "@clerk/nextjs";
+import { useEffect } from "react";
+
+const AMPLITUDE_API_KEY = process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY!;
+
+type Amplitude = typeof amplitudeBrowser;
+export let amplitude: Amplitude;
+
+const useAmplitudeInit = () => {
+  const { user } = useUser();
+
+  useEffect(() => {
+    const initAmplitude = async () => {
+      if (!amplitude) {
+        amplitude = amplitudeBrowser;
+        amplitude.init(AMPLITUDE_API_KEY, undefined, {
+          logLevel: amplitude.Types.LogLevel.Warn,
+        });
+        if (user) {
+          amplitude.setUserId(user.id);
+        }
+      }
+    };
+    initAmplitude();
+  }, [user]);
+};
+
+export const AmplitudeAnalytics = () => {
+  useAmplitudeInit();
+  return null;
+};
