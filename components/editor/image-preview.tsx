b9119a0278dbabc6e46b995915182056cb3a5f21
Max Freundlich <mfreundlich1@gmail.com>
2023-11-15 11:46:24 -0500

    feat: basic error handling

--- components/editor/image-preview.tsx
+++ components/editor/image-preview.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { Block } from "@/lib/streaming-client/src";
 import { useQuery } from "@tanstack/react-query";
+import { AlertTriangleIcon } from "lucide-react";
 import Image from "next/image";
+import { Alert, AlertDescription, AlertTitle } from "../ui/alert";
 import { Skeleton } from "../ui/skeleton";
 
 const BlockImagePreview = ({ block }: { block: Block }) => {
-  const { data } = useQuery({
+  const { data, isError } = useQuery({
     queryKey: ["block", block.id],
     queryFn: () =>
       fetch(
@@ -13,6 +15,21 @@ const BlockImagePreview = ({ block }: { block: Block }) => {
       ).then((res) => res.blob()),
     staleTime: Infinity,
   });
+
+  if (isError) {
+    return (
+      <div>
+        <Alert variant="destructive">
+          <AlertTriangleIcon className="h-4 w-4" />
+          <AlertTitle>Oh no!</AlertTitle>
+          <AlertDescription>
+            We couldn&apos;t load the preview for this image theme. If the issue
+            persists, please contact us on discord
+          </AlertDescription>
+        </Alert>
+      </div>
+    );
+  }
 
   return (
     <div className="overflow-hidden relative w-44 h-44 mt-1 mb-1 rounded-md">
