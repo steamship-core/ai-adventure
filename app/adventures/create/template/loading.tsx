26ff7b296ee75270db94a2095ea0b97cf4d6dbd4
Max Freundlich <mfreundlich1@gmail.com>
2023-11-21 11:11:07 -0500

    feat: improved loading state

--- app/adventures/create/template/loading.tsx
+++ app/adventures/create/template/loading.tsx
@@ -1,20 +1,16 @@
-import { Skeleton } from "@/components/ui/skeleton";
-
 export default function Loading() {
   return (
-    <div className="flex flex-col gap-6 p-4 px-4 md:px-6 py-8">
-      <div className="flex gap-2 flex-col">
-        <Skeleton className="h-[50px] w-[350px]" />
-        <Skeleton className="h-[25px] w-[450px]" />
+    <div className="flex items-center justify-center h-full flex-col gap-8">
+      <div className="honeycomb">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
-      <div className="flex gap-2">
-        <Skeleton className="h-[40px] w-[80px]" />
-        <Skeleton className="h-[40px] w-[80px]" />
-      </div>
-      <div className="flex gap-2">
-        <Skeleton className="h-[550px] w-[166px]" />
-        <Skeleton className="h-[850px] w-full" />
-      </div>
+      Creating your adventure template...
     </div>
   );
 }
