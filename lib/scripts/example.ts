6b51891bd6cb92f6f31bf2ec3a312ff375101be3
Dave Kolas <4643013+dkolas@users.noreply.github.com>
2023-11-21 12:37:41 -0500

    Get most cost data

--- lib/scripts/example.ts
+++ lib/scripts/example.ts
@@ -3,12 +3,16 @@ import { getSteamshipClient } from "../utils";
 
 const fetchData = async () => {
   console.log("These are all the game instances");
-  const agents = await prisma.agents.findMany({});
-  console.log(agents);
+  var agents = await prisma.agents.findMany({});
+  console.log(agents.length);
 
-  console.log("These are all the games");
-  const adventures = await prisma.adventure.findMany({});
-  console.log(adventures);
+  agents = agents.filter((x) => x.workspaceHandle != null);
+  console.log(`Games with workspace handle: ${agents.length}`);
+  console.log("An example:");
+  console.log(agents[0]);
+  // console.log("These are all the games");
+  // const adventures = await prisma.adventure.findMany({});
+  // console.log(adventures);
 
   console.log("Let's talk to steamship");
 
@@ -18,23 +22,38 @@ const fetchData = async () => {
 
   console.log("Let's get our usage summary");
   // Now let's call a method on steamship
-  const resp = await client.get("/account/getUsageSummary");
-  const respJson = await resp.json();
-  console.log(respJson);
-
-  console.log("Let's get usage records");
-  const usageResp = await client.post("/account/usage", {
-    // Can set these to a value to get a specific page
-    pageSize: undefined,
-    pageToken: undefined,
-    sortOrder: undefined,
-    sortOrderOrDefault: undefined,
+  const resp = await client.post("/usage/aggregate", {
+    aggregateField: "billingCredits",
+    aggregation: "sum",
   });
-  const usageRespJson = await usageResp.json();
-  console.log(usageRespJson);
+  const respJson = await resp.json();
+  const costAggregations = respJson["data"]["aggregateResults"];
+  console.log(`Aggregated usages: ${costAggregations.length}`);
 
-  console.log("Let's print out an example of that");
-  console.log(JSON.stringify(usageRespJson?.data?.usage[0], null, 2));
+  console.log("Example aggregated usage:");
+  console.log(costAggregations[0]);
+
+  agents.forEach((agent) => {
+    const agentCosts = costAggregations.filter(
+      (x) => x.workspaceHandle === agent.workspaceHandle
+    );
+    const cost = agentCosts.reduce((sum, item) => sum + item.billingCredits, 0);
+    console.log(`${agent.workspaceHandle} ${cost}`);
+  });
+
+  // console.log("Let's get usage records");
+  // const usageResp = await client.post("/account/usage", {
+  //   // Can set these to a value to get a specific page
+  //   pageSize: undefined,
+  //   pageToken: undefined,
+  //   sortOrder: undefined,
+  //   sortOrderOrDefault: undefined,
+  // });
+  // const usageRespJson = await usageResp.json();
+  // console.log(usageRespJson);
+
+  // console.log("Let's print out an example of that");
+  // console.log(JSON.stringify(usageRespJson?.data?.usage[0], null, 2));
 };
 
 fetchData();
