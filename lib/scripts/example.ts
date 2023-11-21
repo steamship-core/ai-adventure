e54bb2ebe94420ab4ea4959df4661399d0a91060
Dave Kolas <4643013+dkolas@users.noreply.github.com>
2023-11-21 13:29:42 -0500

    Output costs as csv

--- lib/scripts/example.ts
+++ lib/scripts/example.ts
@@ -2,25 +2,26 @@ import prisma from "../db";
 import { getSteamshipClient } from "../utils";
 
 const fetchData = async () => {
-  console.log("These are all the game instances");
+  // console.log("These are all the game instances");
   var agents = await prisma.agents.findMany({});
-  console.log(agents.length);
+  //console.log(agents.length);
 
   agents = agents.filter((x) => x.workspaceHandle != null);
-  console.log(`Games with workspace handle: ${agents.length}`);
-  console.log("An example:");
-  console.log(agents[0]);
+  // console.log(`Games with workspace handle: ${agents.length}`);
+  // console.log("An example:");
+  // console.log(agents[0]);
+
   // console.log("These are all the games");
   // const adventures = await prisma.adventure.findMany({});
   // console.log(adventures);
 
-  console.log("Let's talk to steamship");
+  // console.log("Let's talk to steamship");
 
   // Now let's do something with steamship.
   // This will use your .env.local env vars if called from the script in package.json
   const client = await getSteamshipClient();
 
-  console.log("Let's get our usage summary");
+  // console.log("Let's get our usage summary");
   // Now let's call a method on steamship
   const resp = await client.post("/usage/aggregate", {
     aggregateField: "billingCredits",
@@ -28,17 +29,30 @@ const fetchData = async () => {
   });
   const respJson = await resp.json();
   const costAggregations = respJson["data"]["aggregateResults"];
-  console.log(`Aggregated usages: ${costAggregations.length}`);
+  // console.log(`Aggregated usages: ${costAggregations.length}`);
 
-  console.log("Example aggregated usage:");
-  console.log(costAggregations[0]);
+  // console.log("Example aggregated usage:");
+  // console.log(costAggregations[0]);
 
+  console.log(
+    "ownerId,agentVersion,createdAt,workspaceHandle,plugin,billingCredits"
+  );
   agents.forEach((agent) => {
     const agentCosts = costAggregations.filter(
-      (x) => x.workspaceHandle === agent.workspaceHandle
+      (x: { workspaceHandle: string | null }) =>
+        x.workspaceHandle === agent.workspaceHandle
     );
-    const cost = agentCosts.reduce((sum, item) => sum + item.billingCredits, 0);
-    console.log(`${agent.workspaceHandle} ${cost}`);
+    agentCosts.forEach((cost: { resourceHandle: any; billingCredits: any }) => {
+      if (cost.billingCredits != 0) {
+        console.log(
+          `${agent.ownerId},${
+            agent.agentVersion
+          },${agent.createdAt.toISOString()},${agent.workspaceHandle},${
+            cost.resourceHandle
+          },${cost.billingCredits}`
+        );
+      }
+    });
   });
 
   // console.log("Let's get usage records");
