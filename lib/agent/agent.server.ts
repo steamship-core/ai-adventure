e5dfaaae47634527c7a82c65d3effc1bc8121efb
Ted Benson <ted@steamship.com>
2023-12-03 11:34:16 -0500

    Agent Pool

--- lib/agent/agent.server.ts
+++ lib/agent/agent.server.ts
@@ -1,3 +1,4 @@
+import { Adventure, Agents, AvailableAgents } from "@prisma/client";
 import { log } from "next-axiom";
 import { v4 as uuidv4 } from "uuid";
 import { pushAdventureToAgent } from "../adventure/adventure-agent.server";
@@ -51,19 +52,23 @@ export const deleteAgent = async (userId: string) => {
   return res.count;
 };
 
-export const createAgent = async (
-  userId: string,
-  adventureId: string,
-  isDevelopment: boolean = false
-) => {
-  console.log(
-    `createAgent -  UserId ${userId}; AdventureId ${adventureId}; isDevelopment ${isDevelopment}`
-  );
-  log.info(
-    `createAgent -  UserId ${userId}; AdventureId ${adventureId}; isDevelopment ${isDevelopment}`
-  );
+export const deleteAvailableAgent = async (id: number) => {
+  let res = await prisma.availableAgents.deleteMany({
+    where: {
+      id: id,
+    },
+  });
+  return res.count;
+};
 
-  const adventure = await getAdventure(adventureId);
+export const createAgentInSteamship = async (
+  adventure: Adventure,
+  isDevelopment: boolean
+) => {
+  const adventureId = adventure.id;
+
+  console.log(`createAgentInSteamship -  AdventureId ${adventure.id}`);
+  log.info(`createAgentInSteamship -  AdventureId ${adventure.id}`);
 
   if (!adventure) {
     log.error(`Failed to get adventure: ${adventureId}`);
@@ -123,37 +128,23 @@ export const createAgent = async (
 
     const agentUrl = packageInstance.invocationURL;
 
-    const agentData = {
-      ownerId: userId!,
-      agentUrl: agentUrl,
-      handle: workspaceHandle,
-      adventureId: adventureId,
-      isDevelopment: isDevelopment,
-      agentVersion: adventure.agentVersion,
-      workspaceHandle: workspaceHandle,
-      workspaceId: packageInstance.workspaceId,
-    };
-
-    log.info(`New agent: ${JSON.stringify(agentData)}`);
-    const agent = await prisma.agents.create({ data: agentData });
-
-    if (!agent) {
-      log.error("Agent creation in Prisma failed.");
-      console.log("Agent creation in Prisma failed.");
-      return null;
-    }
     // Now we need to await the agent's startup loop. This is critical
     // because if we perform an operation to quickly after initialization it will fail.
     await steamship.package.waitForInit(packageInstance);
 
     // Now we need to set the server settings.
-    await pushAdventureToAgent(agent.agentUrl, adventure, isDevelopment);
+    await pushAdventureToAgent(agentUrl, adventure, isDevelopment);
 
-    await sendSlackMessage(
-      `🎲 User ${userId} just started a new game: ${adventure.name}!`
-    );
+    const agentData = {
+      agentUrl: agentUrl,
+      handle: workspaceHandle,
+      adventureId: adventureId,
+      agentVersion: adventure.agentVersion,
+      workspaceHandle: workspaceHandle,
+      workspaceId: packageInstance.workspaceId,
+    };
 
-    return agent;
+    return agentData;
   } catch (e) {
     log.error(`${e}`);
     console.log(`Error: ${e}`);
@@ -185,6 +176,174 @@ export const getSchema = async (agentBase: string) => {
   } catch (e) {
     log.error(`${e}`);
     console.error(e);
+    throw Error("Failed to create agent.");
+  }
+};
+
+// Creates an agent that is available and waiting for hot-swap use
+export const createAvailableAgent = async (
+  adventure: Adventure,
+  isDevelopment: boolean,
+  onboardingDataJson: Record<string, any> | undefined = undefined
+) => {
+  const adventureId = adventure.id;
+
+  console.log(`createAvailableAgent -  AdventureId ${adventureId}`);
+  log.info(`createAvailableAgent -  AdventureId ${adventureId}`);
+
+  try {
+    const _agentData = await createAgentInSteamship(adventure, isDevelopment);
+
+    const _onboardingDataJson = onboardingDataJson || {};
+
+    // Stringify in a way that always prints the keys in the right order
+    const onboardedData = JSON.stringify(
+      _onboardingDataJson,
+      Object.keys(_onboardingDataJson).sort()
+    );
+
+    const agentData = {
+      ..._agentData,
+      onboardedData,
+      isDevelopment,
+    } as any;
+
+    log.info(`New AvailableAgent: ${JSON.stringify(agentData)}`);
+    const agent = await prisma.availableAgents.create({ data: agentData });
+
+    if (!agent) {
+      log.error("AvailableAgent creation in Prisma failed.");
+      console.log("AvailableAgent creation in Prisma failed.");
+      return null;
+    }
+
+    return agent;
+  } catch (e) {
+    log.error(`${e}`);
+    console.log(`Error: ${e}`);
+    throw Error("Failed to create agent.");
+  }
+};
+
+export const getAvailableAgent = async (
+  adventureId: string,
+  agentVersion: string,
+  isDevelopment: boolean,
+  onboardingDataJson: Record<string, any> | undefined = undefined
+): Promise<AvailableAgents | null> => {
+  const _onboardingDataJson = onboardingDataJson || {};
+
+  // Stringify in a way that always prints the keys in the right order
+  const onboardedData = JSON.stringify(
+    _onboardingDataJson,
+    Object.keys(_onboardingDataJson).sort()
+  );
+
+  return await prisma.availableAgents.findFirst({
+    where: {
+      adventureId,
+      agentVersion,
+      onboardedData,
+      isDevelopment,
+    },
+    include: {
+      Adventure: true,
+    },
+  });
+};
+
+export const createAgent = async (
+  userId: string,
+  adventureId: string,
+  isDevelopment: boolean = false,
+  useAvailableAgentCache = false,
+  onboardingDataJson: Record<string, any> | undefined = undefined
+) => {
+  console.log(
+    `createAgent -  UserId ${userId}; AdventureId ${adventureId}; isDevelopment ${isDevelopment}; useAvailableAgentCache ${useAvailableAgentCache}`
+  );
+  log.info(
+    `createAgent -  UserId ${userId}; AdventureId ${adventureId}; isDevelopment ${isDevelopment}; useAvailableAgentCache ${useAvailableAgentCache}`
+  );
+
+  try {
+    const adventure = await getAdventure(adventureId);
+
+    let availableAgent: AvailableAgents | null = null;
+
+    if (useAvailableAgentCache) {
+      availableAgent = await getAvailableAgent(
+        adventureId,
+        adventure.agentVersion,
+        isDevelopment,
+        onboardingDataJson
+      );
+    }
+
+    let agent: Agents | undefined = undefined;
+    let agentData: any | undefined = undefined;
+
+    if (availableAgent) {
+      // We need to swap this over to the agent table!
+      agentData = {
+        agentUrl: availableAgent.agentUrl,
+        handle: availableAgent.handle,
+        agentVersion: availableAgent.agentVersion,
+        adventureId: availableAgent.adventureId,
+        workspaceId: availableAgent.workspaceId,
+        workspaceHandle: availableAgent.workspaceHandle,
+        ownerId: userId!,
+        isDevelopment: isDevelopment,
+        completeOnboardingCalled: availableAgent.completeOnboardingCalled,
+      };
+      log.info(
+        `Deleting AvailableAgent and turning to Agent. AvailableAgentId=${
+          availableAgent.id
+        } JSON: ${JSON.stringify(agentData)}`
+      );
+
+      await prisma.availableAgents.delete({
+        where: {
+          id: availableAgent.id,
+        },
+      });
+    } else {
+      // We need to create a new agent from scratch!
+      const _agentData = await createAgentInSteamship(adventure, isDevelopment);
+      agentData = {
+        ..._agentData,
+        ownerId: userId!,
+        isDevelopment: isDevelopment,
+      } as any;
+      log.info(
+        `Creating entirely new agent; no cached one existed: ${JSON.stringify(
+          agentData
+        )}`
+      );
+    }
+
+    if (!agentData) {
+      log.error("Agent creation in Prisma failed: no agentData.");
+      console.log("Agent creation in Prisma failed: no agentData.");
+      return null;
+    }
+
+    agent = await prisma.agents.create({ data: agentData });
+
+    if (!agent) {
+      log.error("Agent creation in Prisma failed.");
+      console.log("Agent creation in Prisma failed.");
+      return null;
+    }
+
+    await sendSlackMessage(
+      `🎲 User ${userId} just started a new game: ${adventure.name}!`
+    );
+
+    return agent;
+  } catch (e) {
+    log.error(`${e}`);
+    console.log(`Error: ${e}`);
     throw Error("Failed to create agent.");
   }
 };
