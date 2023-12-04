79608cb3c17ea9e591fcfdba0f2c76b506ccf0ce
Max Freundlich <mfreundlich1@gmail.com>
2023-12-04 13:49:58 -0500

    fix: remove visible scrollbar

--- components/adventures/character-templates-section.tsx
+++ components/adventures/character-templates-section.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { amplitude } from "@/lib/amplitude";
 import { Character } from "@/lib/game/schema/characters";
 import { Adventure } from "@prisma/client";
 import { TypographyH2 } from "../ui/typography/TypographyH2";
@@ -19,18 +18,6 @@ const CharacterTemplatesSection = ({ adventure }: { adventure: Adventure }) => {
   const playerSingularNoun =
     getAgentConfig(adventure)?.adventure_player_singular_noun || "Player";
 
-  const hasPremadeCharacters = characters.length > 0;
-
-  const onClick = () => {
-    amplitude.track("Button Click", {
-      buttonName: "Start Adventure",
-      location: "Adventure",
-      action: "start-adventure",
-      adventureId: adventure.id,
-      templateCharacter: false,
-    });
-  };
-
   return (
     <>
       <div className="mt-6 flex flex-col w-full h-full items-center justify-center gap-8 ">
@@ -43,7 +30,7 @@ const CharacterTemplatesSection = ({ adventure }: { adventure: Adventure }) => {
             profile will influence gameplay and outcomes.
           </TypographyMuted>
         </div>
-        <div className="overflow-scroll max-h-[50vh]">
+        <div className="overflow-auto max-h-[50vh]">
           <CharacterMap
             characters={[
               // @ts-ignore
