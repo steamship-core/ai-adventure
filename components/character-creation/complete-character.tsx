a5f449676c930fa4c83af67eb2b95d49078c3c27
Max Freundlich <mfreundlich1@gmail.com>
2023-12-06 18:29:18 -0500

    feat: remove unused components

--- components/character-creation/complete-character.tsx
+++ components/character-creation/complete-character.tsx
@@ -1,7 +1,5 @@
-import { getGameState } from "@/lib/game/game-state.client";
 import { GameState } from "@/lib/game/schema/game_state";
 import { AlertTriangle } from "lucide-react";
-import { useParams, useRouter } from "next/navigation";
 import { useRef, useState } from "react";
 import { Alert, AlertDescription, AlertTitle } from "../ui/alert";
 import { Button } from "../ui/button";
@@ -21,45 +19,9 @@ export type CharacterConfig =
     };
 
 const CharacterCreationComplete = ({ config }: { config: CharacterConfig }) => {
-  const router = useRouter();
   const ref = useRef<HTMLButtonElement>(null);
   const [error, setError] = useState<string | null>(null);
-  const params = useParams<{ handle: string }>();
 
-  const pollForAgentSideOnboardingComplete = async () => {
-    // Call getGameState once every second until the game state is no longer onboarding
-    // or an error occurs. Attempt this for 3 minutes.
-    let count = 0;
-    const interval = setInterval(async () => {
-      count += 1;
-      console.log(`polling for agent side onboarding complete count: ${count}`);
-      const gameState = await getGameState(params.handle);
-      let isError = false;
-      if (count > 180) {
-        clearInterval(interval);
-        isError = gameState.active_mode !== "error" ? false : true;
-      } else {
-        isError = gameState.active_mode === "error";
-      }
-      if (isError) {
-        const whatHappened = encodeURIComponent(
-          "Your game has transitioned to an irrecoverable error state."
-        );
-        const whatYouCanDo = encodeURIComponent(
-          "Try creating a new game. We're sorry this happened!"
-        );
-        const technicalDetails = encodeURIComponent(
-          gameState?.unrecoverable_error || "Unknown"
-        );
-        router.push(
-          `/error?whatHappened=${whatHappened}&whatYouCanDo=${whatYouCanDo}&technicalDetails=${technicalDetails}`
-        );
-      } else if (gameState.active_mode !== "onboarding") {
-        clearInterval(interval);
-        router.push(`/play/${params.handle}/camp`);
-      }
-    }, 2000);
-  };
   return (
     <div className="w-full items-start">
       <CreationContent>
