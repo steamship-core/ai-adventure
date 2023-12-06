360c2e2f0eeadb175fd369557f7d4bc6100510b3
Ted Benson <ted@steamship.com>
2023-12-06 11:03:56 -0500

    Chinese period

--- lib/text.ts
+++ lib/text.ts
@@ -1,5 +1,5 @@
 export function addNewlines(text: string): string {
-  const sentenceEndings = /([^\d(Mr)(Mrs)(Ms)(Dr)A-Z][\.!?][\)\]'"”’]* +)/g;
+  const sentenceEndings = /([^\d(Mr)(Mrs)(Ms)(Dr)A-Z][\.。!?][\)\]'"”’]* +)/g;
   const paragraphBreak = "$1\n\n";
   let withNewlines = text.replace(sentenceEndings, paragraphBreak);
   withNewlines = withNewlines.trim();
