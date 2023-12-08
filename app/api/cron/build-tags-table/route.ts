42c6f9431820f05ed258aa65fcc4e493fa9d0a91
Ted Benson <ted@steamship.com>
2023-12-08 12:12:56 -0500

    tag-curation

---
+++ app/api/cron/build-tags-table/route.ts
@@ -0,0 +1,46 @@
+import prisma from "@/lib/db";
+import type { NextRequest } from "next/server";
+
+/**
+ * Transitions as many AvailableAgents with state=waiting to state=ready as possible in appCodeCutoffSeconds seconds.
+ * @param request
+ * @returns
+ */
+export async function GET(request: NextRequest) {
+  const authHeader = request.headers.get("authorization");
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response("Unauthorized", {
+      status: 401,
+    });
+  }
+
+  // Gets all the tags from adventures and builds the Tags table.
+  let tags: Record<string, number> = {};
+  let adventures = await prisma.adventure.findMany({});
+  for (const adventure of adventures) {
+    for (const tag of adventure.tags) {
+      if (tags[tag]) {
+        tags[tag] += 1;
+      } else {
+        tags[tag] = 1;
+      }
+    }
+  }
+
+  for (const tag in tags) {
+    await prisma.tag.upsert({
+      where: {
+        name: tag,
+      },
+      update: {
+        count: tags[tag],
+      },
+      create: {
+        name: tag,
+        count: tags[tag],
+      },
+    });
+  }
+
+  return Response.json({ success: true });
+}
