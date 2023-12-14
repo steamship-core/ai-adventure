2360191d39f1f647ebd19ffca4f62654717e5119
Ted Benson <ted@steamship.com>
2023-12-14 10:43:58 -0500

    Tests

--- app/api/account/post-sign-in/route.tsx
+++ app/api/account/post-sign-in/route.tsx
@@ -10,6 +10,7 @@ export const GET = withAxiom(async (request: Request) => {
   const redirectUrl =
     searchParams.get("redirectUrl") ||
     `${process.env.NEXT_PUBLIC_WEB_BASE_URL}/adventures`;
+  const tryNumber = searchParams.get("tryNumber") || "0";
 
   let _msg = `[post-sign-in] Will redirect to ${redirectUrl}`;
   log.info(_msg);
@@ -22,8 +23,26 @@ export const GET = withAxiom(async (request: Request) => {
   console.log(_msg);
 
   if (!userId) {
-    log.error("No user");
-    throw new Error("no user");
+    if (tryNumber == "0") {
+      // There seems to be a bug in prod with OAuth (but not magic link) login in which just post-login, the cookies
+      // aren't yet available and so it will appear that you don't have a user. This is a hacky workaround which will
+      // send a redirect to THIS SAME PAGE to try to get the cookies to be available.
+      log.info("No user. Sending redirect to try oauth hack");
+
+      let u = new URL(request.url);
+      u.searchParams.set("tryNumber", "1");
+
+      return NextResponse.redirect(u.toString(), {
+        status: 303,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Request-Method": "POST, GET, OPTIONS",
+        },
+      });
+    } else {
+      log.error("No user");
+      throw new Error("no user");
+    }
   }
 
   const maybeAnonId = getAnonId();
