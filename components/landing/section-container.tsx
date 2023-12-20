b96721155b6c857adc00d05d6a85f4f43ddd2160
Max Freundlich <mfreundlich1@gmail.com>
2023-12-20 12:28:11 -0500

    fix: remove fade in text for SEO concerns

--- components/landing/section-container.tsx
+++ components/landing/section-container.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
-import { useInView } from "react-intersection-observer";
 
 export default function SectionContainer({
   children,
@@ -10,14 +9,10 @@ export default function SectionContainer({
   children: ReactNode;
   className?: string;
 }) {
-  const { inView, ref } = useInView({ threshold: 0.5, triggerOnce: true });
-
   return (
     <div
-      ref={ref}
       className={cn(
         "mx-auto max-w-7xl px-6 pb-32 pt-36 sm:pt-60 lg:px-8 lg:pt-32 mt-16  duration-500 transition-all",
-        inView ? "opacity-100" : "opacity-0",
         className
       )}
     >
