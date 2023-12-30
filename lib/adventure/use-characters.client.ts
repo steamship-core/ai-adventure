63ff5a848de6c1394e35ac0ddb35c7d8ea74c20e
Ted Benson <ted@steamship.com>
2023-12-30 17:39:25 -0500

    Yeah

--- lib/adventure/use-characters.client.ts
+++ lib/adventure/use-characters.client.ts
@@ -8,6 +8,7 @@ export const useAgentConfig = (adventure: Adventure) => {
     characters?: Character[];
     adventure_player_singular_noun?: string;
     forbid_custom_characters?: boolean;
+    skip_character_selection?: boolean;
     adventure_singular_noun?: string;
   };
 };
@@ -25,7 +26,24 @@ export const useAdventureCharacters = (adventure: Adventure): Character[] => {
 
   if (!config) return [];
 
+  const skipCharacterSelection = config.skip_character_selection === true;
+
   const characters = config.characters || [];
+
+  // If we're skipping character selection, we'll add a hard-coded one at the end in case
+  // the game didn't have one already created.
+  const diyCharacter = skipCharacterSelection
+    ? ({
+        name: "Player",
+        description: "An ordinary person.",
+        background: "The main character.",
+        tagline: "The main character",
+      } as Character)
+    : ({
+        name: CUSTOM_CHARACTER_NAME,
+        tagline: "Create your own character",
+        custom: true,
+      } as Character);
 
   const premadeCharactersExist = characters.length > 0;
 
@@ -33,15 +51,7 @@ export const useAdventureCharacters = (adventure: Adventure): Character[] => {
     config.forbid_custom_characters === true && premadeCharactersExist;
 
   const customCharacterOffer =
-    noCustomCharacter && characters.length > 0
-      ? []
-      : [
-          {
-            name: CUSTOM_CHARACTER_NAME,
-            tagline: "Create your own character",
-            custom: true,
-          } as Character,
-        ];
+    noCustomCharacter && characters.length > 0 ? [] : [diyCharacter];
 
   const returnCharacters = [
     // @ts-ignore
